6f5f32fb17d8251367ddba01787719ebcd0944f1
Nii-Armah <stanleyniiarmah7@gmail.com>
2026-03-13 11:52:31 +0000

    Create Service component
    Renders a service within the admin module

---
+++ src/features/admin/components/Service/Service.tsx
@@ -0,0 +1,24 @@
+import styles from "./Service.module.css";
+import { ServiceData } from "@/types";
+
+interface ServiceProps {
+  service: ServiceData;
+}
+
+export default function Service({ service }: ServiceProps) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.details}>
+        <p className={styles.name}>{service.name}</p>
+        <p className={styles.durationAndPrice}>
+          <span>{service.duration} min</span> <span> GHS {service.price}</span>
+        </p>
+      </div>
+
+      <div>
+        <p className={styles.bookingLabel}>Bookings</p>
+        <p className={styles.bookingValue}>{service.bookingsCount}</p>
+      </div>
+    </div>
+  );
+}

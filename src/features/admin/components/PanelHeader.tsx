46d3145453005876c0999be99269aeb09da0f584
Nii-Armah <stanleyniiarmah7@gmail.com>
2026-03-13 12:27:54 +0000

    Update PanelHeader

--- src/features/admin/components/PanelHeader.tsx
+++ src/features/admin/components/PanelHeader.tsx
@@ -5,7 +5,12 @@ interface PanelHeaderProps {
 
 export default function PanelHeader({ title, subtitle }: PanelHeaderProps) {
   return (
-    <div>
+    <div
+      style={{
+        borderBottom: "1px solid rgba(100, 116, 139, 0.3)",
+        paddingBottom: "20px",
+      }}
+    >
       <h2>{title}</h2>
       <p>{subtitle}</p>
     </div>

2c092cebc8a652b33b2b75351e0f6e0a05dd0192
Nii-Armah <stanleyniiarmah7@gmail.com>
2026-03-13 15:31:24 +0000

    Update professional registration mutation hook
    Handles persistence of user credentials upon successful registration

--- src/hooks/useProfessionals.ts
+++ src/hooks/useProfessionals.ts
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 
 import { notifications } from "@mantine/notifications";
 import { useMutation } from "@tanstack/react-query";
+import Cookie from "js-cookie";
 
 export const useCreateProfessional = () => {
   const router = useRouter();
@@ -11,7 +12,13 @@ export const useCreateProfessional = () => {
   return useMutation({
     mutationFn: ProfessionalService.create,
     onSuccess: (data) => {
-      console.log("Registration Response: ", data);
+      Cookie.set("access_token", data.tokens.access_token, {
+        expires: 7,
+        path: "/",
+      });
+
+      Cookie.set("user_role", "admin", { expires: 7, path: "/" });
+
       notifications.show({
         title: "Registration Success",
         message: "Your professional profile has been created.",

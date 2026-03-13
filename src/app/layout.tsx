c1f46e0d41a320142709b88bb6c7f4bff4f4a72d
Nii-Armah <stanleyniiarmah7@gmail.com>
2026-03-13 15:02:14 +0000

    Configure mantine notification system in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
+import "@mantine/notifications/styles.css";
 import AppProvider from "@/providers/AppProvider";
+import { Notifications } from "@mantine/notifications";
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -38,6 +40,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <MantineProvider defaultColorScheme="dark">
+          <Notifications position="top-right" />
           <AppProvider>{children}</AppProvider>
         </MantineProvider>
       </body>

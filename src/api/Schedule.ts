4370141ab2dd73e9939bc06be11017c5684749e3
Nii-Armah <stanleyniiarmah7@gmail.com>
2026-03-14 06:09:43 +0000

    Configure day-based ordering for schedules

--- src/api/Schedule.ts
+++ src/api/Schedule.ts
@@ -3,10 +3,29 @@ import { camelizeKeys, decamelizeKeys } from "humps";
 
 import { DayScheduleData } from "@/types";
 
+const DAY_ORDER = [
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+  "sunday",
+];
+
 export default class ScheduleService {
   static async getAll(): Promise<DayScheduleData[]> {
     const response = await apiClient.get("/schedules/");
-    return response.data.map((schedule: unknown) => camelizeKeys(schedule));
+    const schedules = response.data.map((schedule: unknown) =>
+      camelizeKeys(schedule),
+    );
+
+    return schedules.sort((a: DayScheduleData, b: DayScheduleData) => {
+      return (
+        DAY_ORDER.indexOf(a.dayOfWeek.toLowerCase()) -
+        DAY_ORDER.indexOf(b.dayOfWeek.toLowerCase())
+      );
+    });
   }
 
   static async update(schedule: DayScheduleData[]) {

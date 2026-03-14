5b741e15108ffc16e0b768b0f1286d476eb2c048
Nii-Armah <stanleyniiarmah7@gmail.com>
2026-03-14 02:28:29 +0000

    Add schedule panel to admin page

--- src/features/admin/AdminPage.tsx
+++ src/features/admin/AdminPage.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ServicePanel from "@/features/admin/components/ServicePanel/ServicePanel";
+import SchedulePanel from "@/features/admin/components/SchedulePanel/SchedulePanel";
 import SideBar from "@/features/admin/components/SideBar/SideBar";
 import styles from "./AdminPage.module.css";
 
@@ -15,6 +16,10 @@ export default function AdminPage() {
         <div className={styles.contentArea}>
           <Tabs.Panel value="services">
             <ServicePanel />
+          </Tabs.Panel>
+
+          <Tabs.Panel value="schedules">
+            <SchedulePanel />
           </Tabs.Panel>
         </div>
       </div>
